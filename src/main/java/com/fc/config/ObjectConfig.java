2fdc354e694593162b2b1e188721b313980f22c0
Tom <1485217094@qq.com>
2021-08-05 09:14:44 +0800

    第六次提交：show方法

---
+++ src/main/java/com/fc/config/ObjectConfig.java
@@ -0,0 +1,8 @@
+package com.fc.config;
+
+import com.fc.controller.GitHubDemoController;
+import org.springframework.context.annotation.Import;
+
+@Import(ObjectConfig.class)
+public class ObjectConfig {
+}
--- src/main/java/com/fc/controller/GitHubDemoController.java
+++ src/main/java/com/fc/controller/GitHubDemoController.java
@@ -1,10 +1,22 @@
 package com.fc.controller;
 
-public class GitHubDemoController {
-    public void show(){
-        System.out.println("提交");
-        System.out.println("提交");
-        System.out.println("提交");
 
+import com.fc.config.ObjectConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hub")
+public class GitHubDemoController {
+    @Autowired
+    ObjectConfig objectConfig;
+
+    @RequestMapping("show")
+    public void show() {
+        System.out.println(objectConfig);
+        System.out.println("提交");
+        System.out.println("提交");
+        System.out.println("提交");
     }
 }
